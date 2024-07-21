aa89f8d8e81d72873b989b44aef795e8652445ea
hakutakaid <hakutakaid@users.noreply.github.com>
2024-07-22 03:25:39 +0700

    LUPA
    Co-authored-by: hakutakaid <hakutakaid@users.noreply.github.com>
    Co-authored-by: malikahmadfahru91 <malikahmadfahru91@users.noreply.github.com>
    Co-authored-by: Banimudin <Banimudin@users.noreply.github.com>
    Co-authored-by: Farudinopik <Farudinopik@users.noreply.github.com>
    Co-authored-by: samulaksez180182828 <samulaksez180182828@users.noreply.github.com>
    Co-authored-by: rizaldevs <rizaldevs@users.noreply.github.com>
    Co-authored-by: ilanmiuati <ilanmiuati@users.noreply.github.com>
    Co-authored-by: DevARajahID <DevARajahID@users.noreply.github.com>
    Co-authored-by: Ev4ness <Ev4ness@users.noreply.github.com>
    Co-authored-by: FatigahRazo <FatigahRazo@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -28,7 +28,7 @@ API_HASH = getenv("API_HASH")
 ## Get it from @Botfather in Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-MUST_JOIN = getenv("MUST_JOIN")
+MUST_JOIN = getenv("MUST_JOIN", "AnonymousX888")
 
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
