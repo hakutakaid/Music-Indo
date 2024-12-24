cdce6827cbf2dfc1305dcc3b54cedf5f83fa3df9
hakutakaid <hakutakaid@users.noreply.github.com>
2024-12-24 09:17:35 +0700

    finally

--- MusicIndo/core/userbot.py
+++ MusicIndo/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("CariTemanSahabatOnline_Id")
-                await self.one.join_chat("SpotifyStream_Id")               
+                await self.one.join_chat("ZeebSupport")
+                await self.one.join_chat("hakutakaid_gh")               
             except:
                 pass
             assistants.append(1)
@@ -73,8 +73,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("CariTemanSahabatOnline_Id")
-                await self.two.join_chat("SpotifyStream_Id")
+                await self.two.join_chat("ZeebSupport")
+                await self.two.join_chat("hakutakaid_gh")
             except:
                 pass
             assistants.append(2)
@@ -98,8 +98,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("CariTemanSahabatOnline_Id")
-                await self.three.join_chat("SpotifyStream_Id")
+                await self.three.join_chat("ZeebSupport")
+                await self.three.join_chat("hakutakaid_gh")
             except:
                 pass
             assistants.append(3)
@@ -123,8 +123,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("CariTemanSahabatOnline_Id")
-                await self.four.join_chat("SpotifyStream_Id")
+                await self.four.join_chat("ZeebSupport")
+                await self.four.join_chat("hakutakaid_gh")
             except:
                 pass
             assistants.append(4)
@@ -148,8 +148,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("CariTemanSahabatOnline_Id")
-                await self.five.join_chat("SpotifyStream_Id")
+                await self.five.join_chat("ZeebSupport")
+                await self.five.join_chat("hakutakaid_gh")
             except:
                 pass
             assistants.append(5)
