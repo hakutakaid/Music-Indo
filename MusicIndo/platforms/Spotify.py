edded716b69f025f16847183499e5fd71e75b070
hakutakaid <hakutakaid@users.noreply.github.com>
2024-12-24 07:34:15 +0700

    bebsbs

--- MusicIndo/platforms/Spotify.py
+++ MusicIndo/platforms/Spotify.py
@@ -1,29 +1,17 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/MusicIndo > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/MusicIndo/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import re
-
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from youtubesearchpython.__future__ import VideosSearch
-
 import config
 from MusicIndo.utils.decorators import asyncify
 
-
-class Spotify:
+class SpotifyAPI:
     def __init__(self):
         self.regex = r"^(https:\/\/open.spotify.com\/)(.*)$"
-        self.client_id = config.SPOTIFY_CLIENT_ID
-        self.client_secret = config.SPOTIFY_CLIENT_SECRET
-        if config.SPOTIFY_CLIENT_ID and config.SPOTIFY_CLIENT_SECRET:
+        self.client_id: str = config.SPOTIFY_CLIENT_ID
+        self.client_secret: str = config.SPOTIFY_CLIENT_SECRET
+
+        if self.client_id and self.client_secret:
             self.client_credentials_manager = SpotifyClientCredentials(
                 self.client_id, self.client_secret
             )
@@ -33,75 +21,85 @@ class Spotify:
         else:
             self.spotify = None
 
-    async def valid(self, link: str):
-        if re.search(self.regex, link):
-            return True
-        else:
-            return False
+    async def valid(self, link: str) -> bool:
+        return bool(re.search(self.regex, link))
 
-    async def track(self, link: str):
-        track = self.spotify.track(link)
-        info = track["name"]
-        for artist in track["artists"]:
-            fetched = f' {artist["name"]}'
-            if "Various Artists" not in fetched:
-                info += fetched
-        results = VideosSearch(info, limit=1)
-        for result in (await results.next())["result"]:
-            ytlink = result["link"]
-            title = result["title"]
-            vidid = result["id"]
-            duration_min = result["duration"]
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        track_details = {
-            "title": title,
-            "link": ytlink,
-            "vidid": vidid,
-            "duration_min": duration_min,
-            "thumb": thumbnail,
-        }
-        return track_details, vidid
-
-    @asyncify
-    def playlist(self, url: str) -> tuple:
-        playlist = self.spotify.playlist(url)
-        playlist_id = playlist["id"]
-        results = []
-        for item in playlist["tracks"]["items"]:
-            music_track = item["track"]
-            info = music_track["name"]
-            for artist in music_track["artists"]:
-                fetched = f' {artist["name"]}'
+    async def track(self, link: str) -> tuple:
+        try:
+            track = self.spotify.track(link)
+            info = track["name"]
+            for artist in track["artists"]:
+                fetched = f" {artist['name']}"
                 if "Various Artists" not in fetched:
                     info += fetched
-            results.append(info)
-        return results, playlist_id
+            results = VideosSearch(info, limit=1)
+            for result in (await results.next())["result"]:
+                ytlink = result["link"]
+                title = result["title"]
+                vidid = result["id"]
+                duration_min = result["duration"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                track_details = {
+                    "title": title,
+                    "link": ytlink,
+                    "vidid": vidid,
+                    "duration_min": duration_min,
+                    "thumb": thumbnail,
+                }
+                return track_details, vidid
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
 
-    @asyncify
-    def album(self, url: str) -> tuple:
-        album = self.spotify.album(url)
-        album_id = album["id"]
-        results = []
-        for item in album["tracks"]["items"]:
-            info = item["name"]
-            for artist in item["artists"]:
-                fetched = f' {artist["name"]}'
-                if "Various Artists" not in fetched:
-                    info += fetched
-            results.append(info)
-        return results, album_id
+    async def playlist(self, url: str) -> tuple:
+        try:
+            playlist = self.spotify.playlist(url)
+            playlist_id = playlist["id"]
+            results = []
+            for item in playlist["tracks"]["items"]:
+                music_track = item["track"]
+                info = music_track["name"]
+                for artist in music_track["artists"]:
+                    fetched = f" {artist['name']}"
+                    if "Various Artists" not in fetched:
+                        info += fetched
+                results.append(info)
+            return results, playlist_id
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
 
-    @asyncify
-    def artist(self, url: str) -> tuple:
-        artist_info = self.spotify.artist(url)
-        artist_id = artist_info["id"]
-        results = []
-        artist_top_tracks = self.spotify.artist_top_tracks(url)
-        for item in artist_top_tracks["tracks"]:
-            info = item["name"]
-            for artist in item["artists"]:
-                fetched = f' {artist["name"]}'
-                if "Various Artists" not in fetched:
-                    info += fetched
-            results.append(info)
-        return results, artist_id
+    async def album(self, url: str) -> tuple:
+        try:
+            album = self.spotify.album(url)
+            album_id = album["id"]
+            results = []
+            for item in album["tracks"]["items"]:
+                info = item["name"]
+                for artist in item["artists"]:
+                    fetched = f" {artist['name']}"
+                    if "Various Artists" not in fetched:
+                        info += fetched
+                results.append(info)
+            return results, album_id
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+
+    async def artist(self, url: str) -> tuple:
+        try:
+            artist_info = self.spotify.artist(url)
+            artist_id = artist_info["id"]
+            results = []
+            artist_top_tracks = self.spotify.artist_top_tracks(url)
+            for item in artist_top_tracks["tracks"]:
+                info = item["name"]
+                for artist in item["artists"]:
+                    fetched = f" {artist['name']}"
+                    if "Various Artists" not in fetched:
+                        info += fetched
+                results.append(info)
+            return results, artist_id
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
